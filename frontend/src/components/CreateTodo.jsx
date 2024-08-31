86c4b2f87ffb850e15dcdbbe95f80a66eba01296
Akshat Prasad <prasadakshat2@gmail.com>
2024-08-31 12:10:42 +0530

    Update CreateTodo.jsx

--- frontend/src/components/CreateTodo.jsx
+++ frontend/src/components/CreateTodo.jsx
@@ -1,11 +1,35 @@
+import { useState } from "react"
 
 
 export function CreateTodo(){
+
+    const [title, setTitle] = useState("")
+    const [des, setDes]= useState("")
+
     return (
         <div>
-            <input type="text" placeholder="Title"></input><br></br>
-            <input type="text" placeholder="Description"></input><br></br>
-            <button>Add To Do</button>
+            <input type="text" placeholder="Title" onChange={function(e){
+                setTitle(e.target.value)
+            }}></input><br></br>
+            <input type="text" placeholder="Description" onChange={function(e){
+                setDes(e.target.value)
+            }}></input><br></br>
+            <button onClick={()=>{
+                fetch("http://localhost:3000/todo",{
+                    method:"POST",
+                    body: JSON.stringify({
+                        title: title,
+                        description: des
+                    }),
+                    headers:{
+                        "Content-type": "application/json"
+                    }
+                })
+                    .then(async function (res) {
+                        const json = await res.json();
+                        alert("Todo added")
+                    })
+            }}>Add To Do</button>
         </div>
     )
 }
